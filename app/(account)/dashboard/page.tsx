0f144373cb13079de1e7d80cd8b39f43a24ee34c
Joaquín Reyes <hola@jrltt.net>
2024-07-03 09:03:07 +0200

    feat(dashboard): show widget

--- app/(account)/dashboard/page.tsx
+++ app/(account)/dashboard/page.tsx
@@ -1,3 +1,13 @@
+import { Suspense } from "react";
+import { WeatherWidget } from "../components/weather-widget";
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <WeatherWidget location="Bilbao" />
+      </Suspense>
+    </>
+  );
 }
