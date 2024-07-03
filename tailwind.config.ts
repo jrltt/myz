60909422dfede64b287ecd4d4487e6044ba4b339
Joaquín Reyes <hola@jrltt.net>
2024-07-03 08:51:02 +0200

    feat: add brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "z-blue": "#2167ae",
+        "dark-blue": "#23366f",
+        "mid-blue": "#5495cf",
+        "light-blue": "#91bfe3",
+        "sky-blue": "#1fb1e6",
+        "sand-stone": "#dad2bd",
+        dove: "#dde4e3",
+        "z-white": "#eceeef",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
