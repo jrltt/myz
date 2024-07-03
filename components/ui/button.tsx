5d74fbaac6798aff06f20eb9d3e0b5daec173988
Joaquín Reyes <hola@jrltt.net>
2024-07-03 11:14:23 +0200

    feat(ui): update styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,10 +19,12 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         "z-blue": "bg-dark-blue text-white rounded-[2rem]",
+        "z-white":
+          "bg-white text-z-blue border border-2  border-z-blue rounded-[2rem]",
       },
       size: {
         default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
+        sm: "h-9 px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
