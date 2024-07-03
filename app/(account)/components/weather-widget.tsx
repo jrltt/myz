400e81d2b9078b62fb38574c4073caaa60f7829c
Joaquín Reyes <hola@jrltt.net>
2024-07-03 09:54:26 +0200

    feat(weather widget): print more values

--- app/(account)/components/weather-widget.tsx
+++ app/(account)/components/weather-widget.tsx
@@ -3,6 +3,12 @@ import { getWeather } from "@/lib/weather.service";
 export async function WeatherWidget({ location }: { location: string }) {
   const data = await getWeather({ location });
   const currentLocation = data?.location;
+  const weatherInformation = data?.current;
 
-  return <pre>{JSON.stringify(currentLocation, null, 2)}</pre>;
+  return (
+    <>
+      <pre>{JSON.stringify(currentLocation, null, 2)}</pre>
+      <pre>{JSON.stringify(weatherInformation, null, 2)}</pre>
+    </>
+  );
 }
